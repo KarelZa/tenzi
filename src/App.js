95fafd61088a1d6cb224fd7694aaffb7d438be23
KarelZa <karelzamazall@gmail.com>
2022-04-28 17:14:14 +0200

    Added finished game message, number of rolls state, slight change of color pallete

--- src/App.js
+++ src/App.js
@@ -1,32 +1,65 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import Die from './components/Die';
 import { nanoid } from 'nanoid';
+import Confetti from 'confetti-react';
+import Button from './components/Button';
 
 function App() {
 	const [diceNum, setDiceNum] = useState(allNewDice());
+	const [hasWon, setHasWon] = useState(false);
+	const [numOfRolls, setNumOfRolls] = useState(0);
+
+	useEffect(() => {
+		const allHeld = diceNum.every((die) => die.isHeld);
+		const firstValue = diceNum[0].value;
+		const allTheSameValue = diceNum.every((die) => die.value === firstValue);
+		if (allHeld && allTheSameValue) {
+			setHasWon(true);
+		}
+	}, [diceNum]);
+
+	console.log(numOfRolls);
+	// Generate a new die
+	function generateNewDie() {
+		return {
+			value: Math.ceil(Math.random() * 6),
+			isHeld: false,
+			id: nanoid(),
+		};
+	}
 
 	// Returns 10 random numbers between 1-6 inclusive
 	function allNewDice() {
 		const diceArray = [];
 		for (let i = 0; i < 10; i++) {
-			diceArray.push({
-				value: Math.ceil(Math.random() * 6),
-				isHeld: false,
-				id: nanoid(),
-			});
+			diceArray.push(generateNewDie());
 		}
 		return diceArray;
 	}
 
 	// Roll the dice
 	function rollBtnHandler() {
-		setDiceNum(allNewDice);
+		if (hasWon) {
+			setHasWon(false);
+			setDiceNum(allNewDice());
+			setNumOfRolls(0);
+		} else {
+			setNumOfRolls((prevNumOfRolls) => prevNumOfRolls + 1);
+			setDiceNum((prevDice) =>
+				prevDice.map((die) => {
+					if (die.isHeld === false) {
+						return generateNewDie();
+					} else {
+						return die;
+					}
+				})
+			);
+		}
 	}
 
 	// To determine which dice want user hold
 	function holdDice(id) {
-		console.log(id);
 		// 1. call Function that sets State
 		setDiceNum((prevDiceNum) =>
 			// 2. map over prevState of diceNum
@@ -50,7 +83,7 @@ function App() {
 	const diceElements = diceNum.map((die) => {
 		// 2ways how to get correct id down to child
 		// 1st - sending anonymous fnc down to die with correct id -> basically emmbeding id as parameter that will be called with the function --> whenever the die is clicked
-		return <Die holdDice={() => holdDice(die.id)} key={die.id} isHeld={die.isHeld} value={die.value} />;
+		return <Die holdDice={() => holdDice(die.id)} key={die.id} isFinished={hasWon} isHeld={die.isHeld} value={die.value} />;
 		// 2nd - sending both fnc and id - seperately --> in die then call function with id argument
 		// return <Die holdDice={holdDice} id={die.id} key={die.id} isHeld={die.isHeld} value={die.value} />;
 	});
@@ -58,10 +91,30 @@ function App() {
 	return (
 		<div className='App'>
 			<main>
-				<div className='dice-wrapper'>{diceElements}</div>
-				<button className='roll-button roll--text' onClick={rollBtnHandler}>
-					ROLL
-				</button>
+				{hasWon ? (
+					<div className='gameFinished'>
+						<Confetti />
+						<span className='gameFinished--Congratz'>Congratulations</span>
+						<p className='gameFinished--message'>
+							It took you <b>{numOfRolls}</b> 🎲 rolls to finish Tenzi.{' '}
+						</p>
+						<Button text='NEW GAME' onClick={rollBtnHandler} />
+					</div>
+				) : (
+					<div className='mainGame'>
+						<div className='tenzi-description'>
+							<h1 className='tenzi--title'>Tenzi</h1>
+							<p className='tenzi--instructions'>
+								Roll until all dice are the same. Click each die to freeze it at its current value between rolls.
+							</p>
+						</div>
+						<div className='dice-wrapper'>{diceElements}</div>
+						{/* <button className='button roll--text' onClick={rollBtnHandler}>
+							ROLL 🎲
+						</button> */}
+						<Button text='ROLL 🎲' onClick={rollBtnHandler} />
+					</div>
+				)}
 			</main>
 		</div>
 	);
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 
 export default function Die(props) {
+	let pips = props.value
+		? Array(props.value)
+				.fill(0)
+				.map((arr, i) => <span className='pip' key={i}></span>)
+		: '';
+
 	const styling = {
-		backgroundColor: props.isHeld ? '#59E391' : '#FFF',
-		fontWeight: props.isHeld ? 500 : 300,
+		backgroundColor: props.isFinished ? '#59E391' : props.isHeld ? '#008adf4c' : '#FFF',
+		fontWeight: props.isFinished ? 500 : props.isHeld ? 500 : 300,
 	};
 
 	return (
-		<div className='die' style={styling} onClick={props.holdDice}>
-			<span className='die--text'>{props.value}</span>
+		<div className='face' style={styling} onClick={props.holdDice}>
+			{/* <span className='die--text '>{props.value}</span> */}
+			{pips}
 		</div>
 	);
 }
